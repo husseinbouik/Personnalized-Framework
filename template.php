4c31926300e77c33cae635d65cd9209c5ea25ae8
husseinbouik <husseinbouik5@gmail.com>
2024-02-29 11:52:44 +0100

    Add Bootstrap CSS and JS CDN links to template.php

--- template.php
+++ template.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    
     <title><?php echo $pageTitle; ?></title>
 </head>
 <body>
@@ -10,11 +13,17 @@
         <h1><?php echo $pageTitle; ?></h1>
     </header>
 
-    <nav>
-        <ul>
-            <li><a href="index.php?page=home">Home</a></li>
-            <li><a href="index.php?page=product">Product</a></li>
-            <li><a href="index.php?page=contact">Contact</a></li>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php?page=home">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="index.php?page=product">Product</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="index.php?page=contact">Contact</a>
+            </li>
         </ul>
     </nav>
 
@@ -23,7 +32,10 @@
     </main>
 
     <footer>
-        &copy; <?php echo date('Y'); ?> Your Website
+        &copy; <?php echo date('Y'); ?> hussein's website 
     </footer>
+
+    <!-- Bootstrap JS CDN -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
